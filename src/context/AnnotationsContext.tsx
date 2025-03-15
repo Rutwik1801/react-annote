a8eaa41120a060e642916fb2124783fc887a1b24
rutwik-kt <rutwik@kindtech.co.in>
2025-03-15 22:30:49 +0530

    fixed default context value

--- src/context/AnnotationsContext.tsx
+++ src/context/AnnotationsContext.tsx
@@ -2,7 +2,7 @@ import { createContext} from "react";
 import {AnnotationsContextType } from "../utils/types";
 import { useContext } from "react"
 
-export const AnnotationsContext = createContext<AnnotationsContextType>({annotations: []} as AnnotationsContextType);
+export const AnnotationsContext = createContext<AnnotationsContextType>({annotations: [], handleAddAnnotation: () => {}});
 
 export const useAnnotations = () => {
   return useContext(AnnotationsContext);
